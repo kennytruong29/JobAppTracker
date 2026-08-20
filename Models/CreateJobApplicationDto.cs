65196f369c64f6c19c2d550f57a92178c14dc08d
unknown <kennytruong29@yahoo.com>
2026-08-19 18:33:58 -0700

    Add location to table, updated migrations, create GET endpoints and POST endpoint

--- Models/CreateJobApplicationDto.cs
+++ Models/CreateJobApplicationDto.cs
@@ -1,6 +1,18 @@
-﻿namespace JobAppTracker.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobAppTracker.Models
 {
     public class CreateJobApplicationDto
     {
+        [Required]
+        public string CompanyName { get; set; } = string.Empty;
+        [Required]
+        public string JobTitle { get; set; } = string.Empty;
+        [Required]
+        public string Location { get; set; } = string.Empty;
+        public string? URL { get; set; }
+        public string? Notes { get; set; }
+        public string? Contact { get; set; }
+        public Status Status { get; set; } = Status.Applied;
     }
 }
